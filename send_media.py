fc13d919c39de64564a5fdd1ddacbc7ae2226070
Juliano <julianosfreitas@yahoo.com>
2025-06-19 10:05:57 -0300

    Mudando Imagem e Texto do [send_media] e criando o arquivo [chatbot.py] para teste de um novo meio (Evo + Py + Gemini)

--- send_media.py
+++ send_media.py
@@ -13,10 +13,10 @@ payload = {
     'number': '5581985517186',
     'mediatype': 'image',
     'mimetype': 'image/png',
-    'caption': 'Imagem legal',
-    'media': 'https://images.icon-icons.com/2699/PNG/512/python_logo_icon_168886.png',
+    'caption': 'Quando se trata de *decoração de interiores*, *cortina* e *persianas* são ambos elementos amplamente utilizados para controlar a entrada de luz,\n\n garantir *privacidade* e adicionar *beleza* às janelas,\n\n A *Romarri* pode te ajudar na escolha da melhor opção para o seu espaço.',
+    'media': 'https://uniflex.com.br/wp-content/themes/yootheme/cache/32/Tres-scaled-3246b85f.webp',
     'fileName': 'image.png',
-    # 'delay': 10000, # simular "digitando"
+    'delay': 10000, # simular "digitando"
 }
 response = requests.post(
     url=f'{BASE_URL}/message/sendMedia/{INSTANCE_NAME}',
